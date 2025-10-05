eab70af61c0b5e9e5a2c99ddedae339f8bfe2c53
anikaferdous302068 <anika.shesc@gmail.com>
2025-10-05 22:20:46 +0600

    birthday

---
+++ birthday.py
@@ -0,0 +1,11 @@
+print("Here are the birthdays of my 5 friends:\n")
+a = 7,10,2012
+print("Lihan-", a)
+b = 22,6,2012
+print("Merisha-", b)
+c = 28,5,2012
+print("Sumara-", c)
+d= 25,9,2012
+print("Shuprova-", d)
+e= 1,9,2012
+print("Orha-", e)
