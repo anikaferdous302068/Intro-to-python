9f07ea41cd6ed95f6a130156d55ad7b05665f933
anikaferdous302068 <anika.shesc@gmail.com>
2025-11-08 22:32:32 +0600

    yes

---
+++ typecasting.py
@@ -0,0 +1,10 @@
+name = "Kitten" 
+age = 2
+student = True
+weight = 4.5
+print ("data type of name:", type(name))
+print ("data type of age:", type(age))
+print ("data type of student:", type(student))
+print ("data type of weight:", type(weight))
+print ("after typecasting data type of age:", type (str(age)))
+print ("after typecasting data type of weight:", type (int(weight)))
