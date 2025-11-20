9de2ce6661b701e797b3de3f76896e0725dd5b28
anikaferdous302068 <anika.shesc@gmail.com>
2025-11-20 23:06:30 +0600

    done

---
+++ average.py
@@ -0,0 +1,8 @@
+tree1= 98
+tree2= 76
+tree3= 54
+tree4= 32
+tree5= 10
+sum= tree1+ tree2+ tree3+ tree4 + tree5
+average= (tree1+ tree2+ tree3+ tree4+ tree5) / 5
+print("The average height of the trees is:", average)
