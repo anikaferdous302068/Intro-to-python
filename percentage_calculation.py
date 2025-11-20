77533afdfdc7f2e23af4b80e88a92e8abce86509
anikaferdous302068 <anika.shesc@gmail.com>
2025-11-20 23:29:53 +0600

    done

---
+++ percentage_calculation.py
@@ -0,0 +1,9 @@
+print ("Calculate the percentage of 4 subjects:")
+English= int(input('Enter the marks of English that you secured:'))
+Science= int(input("Enter the marks of Science that you secured:"))
+Math= int(input("Enter the marks of Math that you secured:"))
+ICT= int(input("Enter the marks of ICT that you secured:"))
+Total_Marks= English+Science+Math+ICT
+Percentage= (Total_Marks/400)*100
+print(end="Percentage Mark=")
+print (Percentage)
