e2136c24981a0b3d87ff652477cfb04214bdc457
anikaferdous302068 <anika.shesc@gmail.com>
2025-11-27 21:39:36 +0600

    doneeee

---
+++ summer_times.py
@@ -0,0 +1,10 @@
+season=input("Enter the season): ")
+print("The current season is:", season)
+if season=="Summer":
+    print("Wear light clothes and stay hydrated!")
+if season=="Winter":
+    print("Wear warm clothes and enjoy the snow!")
+if season=="Spring":
+    print("Enjoy the blooming flowers and mild weather!")
+if season=="Autumn":
+    print("Enjoy the falling leaves and cooler temperatures!")    
